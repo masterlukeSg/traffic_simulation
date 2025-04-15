3c77b9f92d01ea5d3e442cfae558dd02e738ab09
Lukas Baumeister <baumeister.luke@web.de>
2025-04-15 23:28:54 +0200

    Refactored the main loop. Cars spawn automatically and are deleted when they are out of the window.

--- version_one/src/main.py
+++ version_one/src/main.py
@@ -1,7 +1,7 @@
 import pygame
 from random import randint
 from enum import Enum
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class Color(Enum):
@@ -17,23 +17,27 @@ class ColorPhase(Enum):
     
 
 class Car:
-    def __init__(self, screen, position, color=Color.BLACK):
-        self.radius = 5
+    def __init__(self, screen, x, y, color=Color.BLACK):
+        self.radius = 4.5
         self.speed = 60
         self.screen = screen
-        self.position: list[int,int] = position
         self.color = color.value
-    
+        self.x = x
+        self.y = y
+        
     def draw(self) -> None:
-        pygame.draw.circle(self.screen, self.color, self.position, self.radius)
+        pygame.draw.circle(self.screen, self.color, [self.x, self.y], self.radius)
     
     def move(self, delta_time) -> None:
-        self.position[0] += self.speed * delta_time     
+        self.x += self.speed * delta_time     
     
-    def position(self) -> list[int]:
-        return self.position
+    def position(self) -> tuple[int, int]:
+        return (self.x, self.y)
    
+    def is_off_screen(self) -> bool:
+        return self.x > self.screen.get_width() or self.y > self.screen.get_height()
    
+
 class TrafficLight:
     def __init__(self, screen, x, y, color=ColorPhase.RED):
         self.screen = screen
@@ -57,9 +61,9 @@ class TrafficLight:
         pygame.draw.rect(self.screen, self.color_phase.value, pygame.Rect(self.x, self.y, self.width, self.heihgt))
     
     def update_phase(self) -> None:
-        # Calculate how many full seconds have passed since the game started
+        # Calculate how many full seconds have passed since the traffic light was created
         self.seconds_passed = (pygame.time.get_ticks() - self.start_ticks) // 1000
-        # Calculate how much time is left before the countdown ends (traffic light turns green)
+        # Calculate how much time is left before the countdown ends (traffic light turns red or green)
         self.time_left = max(0, self.countdown_start - self.seconds_passed)
         
         # when 2 and 1 seconds are left: Yellow 
@@ -106,7 +110,7 @@ class TrafficLight:
         # first dict and then go into second dict
         return transition.get(start, {}).get(phase, {})
     
-    def seconds_till_next_phase(self) -> int:
+    def remaining_time(self) -> int:
         red = ColorPhase.RED.name
         green = ColorPhase.GREEN.name
         
@@ -116,8 +120,11 @@ class TrafficLight:
             return int(self.time_left - self.yellow_phase)
         else:        
             return int(self.time_left)
-        
     
+    def get_location(self) -> tuple[int, int]:
+        return (self.x, self.y) 
+    
+
 class Road(ABC):
     def __init__(self, screen, start_x, start_y, lenght, width, lanes_amount=2, lane_width=6):
         self.screen = screen
@@ -170,59 +177,77 @@ class Game():
         self.SCREEN_WIDTH = 1200
         self.SCREEN_HEIGHT = 900
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        
         self.clock = pygame.time.Clock()
         self.prev_time = pygame.time.get_ticks()
-        self.active_game: bool = True
-        self.my_font = pygame.font.SysFont('Freeroad', 30) # for text
-    
+        self.my_font = pygame.font.SysFont('Freeroad', 20)
+
+        self.cars: list[Car] = []
+        self.car_spawn_rate: int = 4 # every ... seconds a car should spawn
+        self.last_car_spawn_time: int = pygame.time.get_ticks() - self.car_spawn_rate * 1000.
+
     def play(self) -> None:
-        self.cars: Car = [Car(self.screen, [60,120]), Car(self.screen, [300,160])]
-        self.traffic_light = TrafficLight(self.screen, 460, 100)
+        self.traffic_light = TrafficLight(self.screen, 500, 390)
         self.h_road = HorizontalRaod(self.screen, 100, 400, 410, 80)
         
         self.game_loop()
     
+    def spawn_car(self, x: int = 120, y: int = 420) -> None:
+        self.cars.append(Car(self.screen, x, y))
+
+    def create_cars(self) -> None:
+        current_time = pygame.time.get_ticks()
+        if (current_time - self.last_car_spawn_time) >= self.car_spawn_rate * 1000:
+            self.spawn_car()
+            self.last_car_spawn_time = current_time
+                  
     def update_time(self, prev_time) -> tuple[int, float]:
         current = pygame.time.get_ticks()
         delta = (current - prev_time) / 1000.0  # Consistent car movement regardless of frame rate
         return delta, current
     
-    def time_left_text(self) -> None:
+    def remaining_time_text(self) -> None:
         # Text
-        time_left = str(self.traffic_light.seconds_till_next_phase())
+        time_left = str(self.traffic_light.remaining_time())
         next_pahse = self.traffic_light.get_next_phase().title()
         combined = f'In {time_left} seconds the traffic light will change to {next_pahse}'
         self.text1 = self.my_font.render(combined, True, Color.BLACK.value)
         self.screen.blit(self.text1,(10, 10))
-            
+    
+    def handle_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+    def update(self) -> None:
+        self.delta_time, self.prev_time = self.update_time(self.prev_time)
+        self.create_cars()
+        self.cars = [car for car in self.cars if not car.is_off_screen()]
+        print(len(self.cars))
+
+        
+        for car in self.cars:
+            car.move(self.delta_time)
+
+    def draw(self) -> None:
+        self.screen.fill(Color.GRAY.value)
+        self.h_road.create()
+        self.traffic_light.draw()
+        
+        for car in self.cars:
+            car.draw()
+        
+        self.remaining_time_text()
+        pygame.display.flip()
+        
     def game_loop(self) -> None:
-        while self.active_game:
+        while True:
             self.clock.tick(60)
+            self.handle_events()
+            self.update()
+            self.draw()
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.active_game = False
-                    pygame.quit()
-
-            # Background color
-            self.screen.fill(Color.GRAY.value)
-            
-            # Update time 
-            self.delta_time, self.prev_time = self.update_time(self.prev_time)    
-            
-            # Draw objetcs
-            self.h_road.create() # create road
-            self.traffic_light.draw() # create traffic light
-            
-            for car in self.cars: # create all cars
-                car.draw()
-                car.move(self.delta_time)
-            
-            # Write how many seconds are left till the traffic light changes
-            self.time_left_text()
-            
-            # Refresh window
-            pygame.display.flip()
 
 
 
